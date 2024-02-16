ee040f65fb4ce72b83cf9da453024b302f77dcbe
anamariaow <new_annemarie@yahoo.com>
2024-02-16 16:29:07 +0100

    Esercizio Controller con @RequestParam

---
+++ src/main/java/com/example/controllerqueryparameter/ControllerQueryParameterApplication.java
@@ -0,0 +1,13 @@
+package com.example.controllerqueryparameter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerQueryParameterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerQueryParameterApplication.class, args);
+	}
+
+}
