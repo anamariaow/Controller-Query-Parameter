c0aa78aa3d64a249a5d721002ab70ae910622898
anamariaow <new_annemarie@yahoo.com>
2024-02-16 16:59:21 +0100

    Aggiunto metodo con calcolo della differenza tra anno corrente ed età

--- src/main/java/com/example/controllerqueryparameter/HomeController.java
+++ src/main/java/com/example/controllerqueryparameter/HomeController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 @RestController
 @RequestMapping("/v1")
 
@@ -14,4 +16,13 @@ public class HomeController {
         return "Ciao " + nome + ", com'è il tempo in " + provincia + "?";
     }
     //localhost:8080/v1/ciao?nome=Giuseppe&provincia=Lombardia
+
+    @GetMapping("/annoDiNascita")
+    public Integer annoDiNascita(@RequestParam String annoNascita) {
+        LocalDate annoCorrente = LocalDate.now();
+        LocalDate dataDiNascita = LocalDate.parse(annoNascita);
+        Integer eta =(annoCorrente.getYear() - dataDiNascita.getYear());
+        return eta;
+    }
+    //localhost:8080/v1/annoDiNascita?annoDiNascita=1997-09-28
 }
